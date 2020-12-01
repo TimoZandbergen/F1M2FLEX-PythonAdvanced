2886e6c18b2c3e20d1a912869f0d773a0ab4effa
Timo Zandbergen <zandbergentimo@gmail.com>
2020-12-01 09:41:44 +0100

    class

---
+++ class.py
@@ -0,0 +1,21 @@
+class Person:
+  def __init__(myself, name, age, hobby):
+    myself.name = name
+    myself.age = age
+    myself.hobby = hobby
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+    print("I like playing " + abc.hobby + " with my soccer team")
+p1 = Person("Timo", 16, "soccer")
+p1.myfunc()
+
+
+
+
+
+        
+
+
+
+
