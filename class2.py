09eb8d3ba624411d0a50803f6dcb4582a1c494f9
Timo Zandbergen <zandbergentimo@gmail.com>
2020-12-14 17:03:16 +0100

    flex

--- class2.py
+++ class2.py
@@ -1,44 +1,54 @@
-# inheritance.py
+# class2.py
 
 class Person:
     var1 = "My name is Timo Zandbergen and i am 16 years old"
-    var2 = 0
+    var2 = "I love playing soccer with my friends"
     var3 = 10.0
     var4 = True
+    
 
     def var4truth(self):
-        print(f"var4 in Class Person is {Person.var4}.")   
+        print(f"Is the question {Person.var4} or False.")   
   
     def printvar1(self):
         print(Person.var1)
 
+    def printvar2(self):
+        print(Person.var2)
+
 
 
 class Student(Person):
-    var5 = [1, 2, 3]
+    var5 = "You have a boost for 5 seconds"
     var6 = range(1, 6)
 
     def __init__(self):
-        var1 = "There is now text here"
+        var1 = "Hello, how are you?"
         var4 = False
     
     def var4truth(self):
-        print(f"var4 is now {Student.var4} in Class Student.")
+        print(f"The question is {Student.var4}.")
     
     def printvar1(self):
         super().printvar1()
-        print("This function was put in class B, and therefore has been updated to display this text by using the super() function.")
+        print("Hello how are you?")
+
+    def printvar2(self):
+        super().printvar2()
+        print("I still like playing soccer with friends")                                                                                                                                                                                                                                                                                                                                                       
+
 
 instanceA = Person()
 instanceB = Student()
 
-print(f"Variable 1 of Class Person is {instanceA.var1}.")
-print(f"Variable 2 of Class Person is {instanceA.var2}.")
-print(f"Variable 3 of Class Person is {instanceA.var3}.")
-print(f"Variable 4 of Class Person is {instanceA.var4}.")
-print(f"Variable 5 of Class Student is {instanceB.var5}.")
-print(f"Variable 6 of Class Student is {instanceB.var6}.")
+print(f"{instanceA.var1}.")
+print(f"{instanceA.var2}.")
+print(f"{instanceA.var3}.")
+print(f"{instanceA.var4}.")
+print(f"{instanceB.var5}.")
+print(f"{instanceB.var6}.")
 
 instanceA.var4truth()
 instanceB.var4truth()
 instanceB.printvar1()
+instanceB.printvar2()
